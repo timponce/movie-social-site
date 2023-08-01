a4eb422ebfe7f3a73f2302fa11be4fe172c2c49e
Timothy Ponce <timothyjponce@gmail.com>
2023-07-31 20:39:34 -0700

    Boilerplate REST API

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import colors from "colors";
 import "dotenv/config";
 import errorHandler from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 import filmRoutes from "./routes/filmRoutes.js";
 const port = process.env.PORT || 5001;
+
+connectDB();
 
 const app = express();
 
